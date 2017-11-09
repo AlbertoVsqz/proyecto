1ac5d84866465c617a7fc3ad657c74b48a3c39b2
Nenii <arguetajenni4@gmail.com>
2017-11-09 15:38:47 -0600

    prueba 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    JENNIIIIIIIIII
+                    Hola k hace?
                 </div>
 
                 <div class="links">
